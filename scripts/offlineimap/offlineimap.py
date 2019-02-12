cb835c7b9f1b12086ab591b383388c9191859179
Willmar Knikker <wil7vin@live.nl>
2019-02-12 09:44:40 +0100

    Push to merge desktop changes

--- scripts/offlineimap/offlineimap.py
+++ scripts/offlineimap/offlineimap.py
@@ -1,8 +1,5 @@
 #! /usr/bin/env python2
 from subprocess import check_output 
 
-def get_pass(email, account):
-    return check_output("gpg -r " + email + " -dq ~/.config/offlineimap/passwords/" + account + ".gpg", shell=True).strip("\n")
-
-def get_email(email, account):
-    return check_output("gpg -r " + email + " -dq ~/.config/offlineimap/emails/" + account + ".gpg", shell=True).strip("\n")
+def get_pass(account):
+    return check_output("pass General/Work/Projects/Server/" + account, shell=True).splitlines()[0]
